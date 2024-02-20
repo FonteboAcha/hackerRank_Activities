9452c15e97e05d501ee4d1ffa246bbd5a3214ca6
fontebo <fonteboachah@gmail.com>
2024-02-20 17:16:11 +0100

    solved python if_else (without test cases)

---
+++ py_if_else1.py
@@ -0,0 +1,52 @@
+# Task
+# Given an integer, n, perform the following conditional actions:
+#
+# If n is odd, print Weird
+# If n is even and in the inclusive range of  to , print Not Weird
+# If n is even and in the inclusive range of  to , print Weird
+# If n is even and greater than , print Not Weird
+# Input Format
+#
+# A single line containing a positive integer, .
+#
+# Constraints
+#
+# Output Format
+#
+# Print Weird if the number is weird. Otherwise, print Not Weird.
+#
+# Sample Input 0
+#
+# 3
+# Sample Output 0
+#
+# Weird
+# Explanation 0
+#
+#
+#  is odd and odd numbers are weird, so print Weird.
+#
+# Sample Input 1
+#
+# 24
+# Sample Output 1
+#
+# Not Weird
+# Explanation 1
+#
+#
+#  and  is even, so it is not weird.
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+if n % 2 == 1:
+    print("Weird")
+
+if n % 2 == 0:
+    if 2 <= n <= 5:
+        print("Not Weird")
+    if 6 <= n <= 20:
+        print("Weird")
+    if n > 20:
+        print("Not Weird")
